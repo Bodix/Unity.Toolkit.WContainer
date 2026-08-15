ab7158d2fa6b836838044e74b289384cd952ec4f
Bodix <bodix321@gmail.com>
2026-08-15 10:00:38 +0300

    Add `BackNavigationConnector`

---
+++ Scripts/BackNavigationConnector.cs
@@ -0,0 +1,30 @@
+﻿using Bodix.Evolunity.Services;
+using UnityEngine;
+using VContainer;
+
+namespace Toolkit.WContainer
+{
+	[RequireComponent(typeof(IBackHandler))]
+	public class BackNavigationConnector : MonoBehaviour
+	{
+		[Inject]
+		private IBackNavigationService _navigationService;
+
+		private IBackHandler _handler;
+
+		private void Awake()
+		{
+			_handler = GetComponent<IBackHandler>();
+		}
+
+		private void OnEnable()
+		{
+			_navigationService?.Register(_handler);
+		}
+
+		private void OnDisable()
+		{
+			_navigationService?.Unregister(_handler);
+		}
+	}
+}
