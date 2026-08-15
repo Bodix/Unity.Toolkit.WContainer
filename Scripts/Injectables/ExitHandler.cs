36a7337707c5a4a0ab27123519b18ff6f8518ce5
Bodix <bodix321@gmail.com>
2026-08-15 11:49:14 +0300

    Add `ExitHandler`. Refactoring

---
+++ Scripts/Injectables/ExitHandler.cs
@@ -0,0 +1,13 @@
+﻿using Bodix.Evolunity.Services;
+using VContainer;
+
+namespace Toolkit.WContainer
+{
+	public class ExitHandler : Bodix.Evolunity.Components.ExitHandler
+	{
+		[Inject]
+		private UiDialogService _uiDialogService;
+
+		protected override UiDialogService UiDialogService => _uiDialogService;
+	}
+}
