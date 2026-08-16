793600c315f7c6003509fcc00c82ba57a4f00789
Bodix <bodix321@gmail.com>
2026-08-16 19:25:49 +0300

    Add instantiator

---
+++ Scripts/VContainerInstantiator.cs
@@ -0,0 +1,22 @@
+﻿using Bodix.Evolunity.Services;
+using UnityEngine;
+using VContainer;
+using VContainer.Unity;
+
+namespace Toolkit.WContainer
+{
+	public class VContainerInstantiator : IInstantiator
+	{
+		private readonly IObjectResolver _resolver;
+
+		public VContainerInstantiator(IObjectResolver resolver)
+		{
+			_resolver = resolver;
+		}
+
+		public T Instantiate<T>(T prefab, Transform parent) where T : Component
+		{
+			return _resolver.Instantiate(prefab, parent);
+		}
+	}
+}
